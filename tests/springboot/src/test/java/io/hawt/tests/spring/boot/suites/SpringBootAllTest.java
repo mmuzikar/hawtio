feac2a1438852b2d4bcaf29512bd4b2d163b889b
Martin Muzikar <mmuzikar@redhat.com>
2023-05-25 09:25:44 +0200

    Fix surefire not picking up tests, enable logging for e2e tests

--- tests/springboot/src/test/java/io/hawt/tests/spring/boot/suites/SpringBootAllTest.java
+++ tests/springboot/src/test/java/io/hawt/tests/spring/boot/suites/SpringBootAllTest.java
@@ -1,15 +1,13 @@
 package io.hawt.tests.spring.boot.suites;
 
-import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
-
-import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
-//@SelectClasspathResource("../../tests/utils/src/main/java/io/hawt/tests/utils/features/")
-@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@springBoot")
+@SelectClasspathResource("io/hawt/tests/utils/features")
+//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@springBoot")
 public class SpringBootAllTest {
 
 }
