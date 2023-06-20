d09babd56a0912544e7aeb001b939a7785bdc282
Martin Muzikar <mmuzikar@redhat.com>
2023-06-20 09:09:25 +0200

    E2E testsuite refactor for easier containerization

---
+++ tests/hawtio-test-suite/src/main/java/io/hawt/tests/features/hooks/LoginLogoutHooks.java
@@ -0,0 +1,28 @@
+package io.hawt.tests.features.hooks;
+
+import com.codeborne.selenide.Selenide;
+
+import io.cucumber.java.Before;
+import io.hawt.tests.features.config.TestConfiguration;
+import io.hawt.tests.features.setup.LoginLogout;
+import io.hawt.tests.features.setup.WebDriver;
+
+public class LoginLogoutHooks {
+
+    private static boolean init = false;
+
+    @Before
+    public static void before() {
+        if (!init) {
+            WebDriver.setup();
+            LoginLogout.login(TestConfiguration.getAppUsername(), TestConfiguration.getAppPassword());
+        } else {
+            if (Selenide.webdriver().driver().getWebDriver().getCurrentUrl().contains("login")) {
+                LoginLogout.login(TestConfiguration.getAppUsername(), TestConfiguration.getAppPassword());
+            } else {
+                Selenide.open(DeployAppHook.getBaseURL() + TestConfiguration.getUrlSuffix() + "/jmx");
+            }
+        }
+        LoginLogout.hawtioIsLoaded();
+    }
+}
